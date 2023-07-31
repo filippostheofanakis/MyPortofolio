f15be1fdc40d2dbee115971d7d794deaed4a2ad2
felipbttk <felipbttk@hotmail.com>
2023-07-31 20:27:27 +0200

    first commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,7 +28,7 @@ export const Projects =  () => {
             title: "Create your QR code",
             description: "Share it with your friends",
             imgUrl: qr,
-            link:  <a href = "https://github.com/felipbttk/Qr_Code_Generator_Reader" target="blank">Click me</a>,
+            link:  <a href = "https://felipbttk.github.io/Qr_Code_Generator_Reader/" target="blank">Click me</a>,
           },
           {
             title: "Find all the E-Mails",
